cf6c5b258e8013296aa358f8f80dbe8519251a91
Aran <138863634+araaancode@users.noreply.github.com>
2024-08-11 00:08:22 +0330

    Update authService.js

--- frontend/src/landing/app/services/auth/authService.js
+++ frontend/src/landing/app/services/auth/authService.js
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
-const baseUrl = "https://deploy-one-backend.onrender.com/"
+const baseUrl = "https://deploy-one-backend.onrender.com"
 
 export const authApi = createApi({
   reducerPath: 'authApi',
